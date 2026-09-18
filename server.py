29214933e7b1975397ae1723b1e6db1bba291531
Your Name <your_email@example.com>
2026-09-18 23:30:03 +0300

    Add Uvicorn dynamic port binding for Render

--- server.py
+++ server.py
@@ -854,3 +854,8 @@ async def _shutdown():
         pass
     if BOT_TASK:
         BOT_TASK.cancel()
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("server:app", host="0.0.0.0", port=port, reload=False)
